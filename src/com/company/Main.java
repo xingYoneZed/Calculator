f2f4a7e9ff9caa6e43672bf1484d587b877ed94f
xingYoneZed <darklordchinchin121212@yandex.ru>
2022-04-16 12:25:18 +0300

    Add project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,22 @@ import java.util.TreeMap;
 
 public class Main {
 
+    public static void chekNumbers(Number n1, Number n2) throws Exception{
+
+        if (n1.getNumber() > 10 | n1.getNumber() < 1){
+            throw new Exception("Wrong number");
+        }
+        if (n2.getNumber() > 10 | n2.getNumber() < 1){
+            throw new Exception("Wrong number");
+        }
+        if (n1.type != n2.type ){
+            throw new Exception("Wrong number");
+        }
+
+
+    }
+
+
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
@@ -16,9 +32,12 @@ public class Main {
 
         String [] symbols = answer.split(" ");
 
+        if (symbols.length > 3) throw new Exception("Hey man!!!! Please stop!");
         Number number1 = new Number(symbols[0]);
         Number number2 = new Number(symbols[2]);
         Number result = new Number(symbols[0]);
+        chekNumbers(number1,number2);
+
         switch (symbols[1]){
             case "+":
                 result.setNumber(number1.getNumber()+number2.getNumber());
